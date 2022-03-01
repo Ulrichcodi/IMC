7604e9a278568b95cce7596176af820ef76a12de
ulrichcodi <ulrichcodi@gmail.com>
2022-03-01 22:34:31 +0100

    IMC participants: ulrich Noumbissi , Franck Steve

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react"
+import { StyleSheet, Text, View, TextInput, Button } from "react-native"
 
 export default function App() {
+  const [taille, setTaille] = useState(150)
+  const [poids, setPoids] = useState(50)
+
+  function calcul() {
+    let p = parseInt(poids)
+    let T = parseInt(taille)
+    res = p / (T * T)
+  }
+  function diag() {
+    let k = parseInt(res)
+    if (k < 18.5) return "Insuffisance pondérale"
+    if (k >= 18.5 && k < 24.9) return "Poids normal"
+    if (k >= 25 && k < 29.9) return "Surpoids"
+    if (k > 30) return "Obésité"
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      <Text>pour calculer votre 'IM', veillez saisir vos informations</Text>
 
+      <TextInput
+        style={styles.input}
+        onChangeText={setPoids}
+        value={poids}
+        placeholder="entrer votre poids en kg"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setTaille}
+        value={taille}
+        placeholder="entrer votre taille en mètre"
+      />
+      <Button title="CALCULER" onPress={() => calcul} />
+      <Text>
+        votre "IMC" est de:{calcul}kg/m²+{diag}
+      </Text>
+    </View>
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+})
